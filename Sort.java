53d3cd007e83e758dc480365e3fadebfb4a3a1d0
Sujal <sadhanasingh8358@gmail.com>
2025-09-19 14:05:47 +0530

    [sujal].add add the file

---
+++ Sort.java
@@ -0,0 +1,37 @@
+public class Sort{
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 1, 2, 0};
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        // Dutch National Flag Algorithm
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0: {
+                    int temp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1: {
+                    mid++;
+                    break;
+                }
+                case 2: {
+                    int temp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp;
+                    high--;
+                    break;
+                }
+            }
+        }
+
+        // Print sorted array
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
